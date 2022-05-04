4f4b7e6d43be4f163ece2564c612795511765805
Kelly Kaufman <kellykaufman12@gmail.com>
2022-05-04 12:17:31 -0700

    Adds all mens clothing womens clothing jewelry and electronics buttons

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -36,10 +36,20 @@ const Loading = () => {
 };
 
 const ShowProducts = () => {
-  <div className="buttons">
+
+  return (
+<>
+<div className="buttons">
     <button className="btn btn-outline-dark">All</button>
-    <button className="btn btn-outline-dark">Men's Clothing</button>
+    <button className="btn btn-outline-dark me-2">Men's Clothing</button>
+    <button className="btn btn-outline-dark me-2">Women's Clothing</button>
+    <button className="btn btn-outline-dark me-2">Jewelry</button>
+    <button className="btn btn-outline-dark me-2">Electronics</button>
   </div>
+</>
+
+  );
+
 };
 
 return (

60ed7d21f7884a8362078a49db541bb0bf4ec1fb
Kelly Kaufman <kellykaufman12@gmail.com>
2022-05-04 12:12:14 -0700

    Adds show products function

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -27,13 +27,32 @@ export const Products = () => {
 getProducts();
 }, []);
 
+const Loading = () => {
+  return (
+    <>
+    Loading....
+    </>
+  )
+};
+
+const ShowProducts = () => {
+  <div className="buttons">
+    <button className="btn btn-outline-dark">All</button>
+    <button className="btn btn-outline-dark">Men's Clothing</button>
+  </div>
+};
+
 return (
   <div>
-    <div className="container">
+    <div className="container my-5 py-5">
       <div className="row">
-        <div className="col-12">
-          <h1>Latest Products</h1>
+        <div className="col-12 mb-5">
+          <h1 className="display-6 fw-bolder text-center">Latest Products</h1>
+          <hr/>
         </div>
+      </div> 
+      <div className="row justify-content-center">
+        {loading ? <Loading/> : <ShowProducts/>}
       </div>
     </div>
   </div>

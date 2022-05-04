9723f38a125b7d8aa13e84842dfc651c8697ca4c
Kelly Kaufman <kellykaufman12@gmail.com>
2022-05-04 08:12:28 -0700

    Adds shopping cart and Register Icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,41 +4,38 @@ export default function  () {
   return (
    
     <div><nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+    <div class="container">
+      <a class="navbar-brand fw-bold fs-4" href="#">Faux Comm</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-      </button>
+      </button> 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+       
+        <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Products</a>
+          </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"/></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
+            <a class="nav-link" href="#">Contact</a>
           </li>
         </ul>
-        <form class="d-flex">
-          <h1  class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button> </h1 >
-        </form>
-      </div>
+     <div className="buttons">
+       <a href="" className="btn btn-outline-dark"> 
+       <i className="fa fa-sign-in-me-1" ></i>Login</a>
+    <a href="" className="btn btn-outline-dark ms-2"> 
+    <i class="fa-solid fa-user-plus me-1"></i>Register</a>
+    <a href="" className="btn btn-outline-dark"> <i className="fa fa-shopping-cart me-1" ></i>Cart(0)</a>
     </div>
-  </nav> </div>
+    </div>
+    </div>
+  </nav> 
+  </div>
 
-  )
-}
+  );
+};

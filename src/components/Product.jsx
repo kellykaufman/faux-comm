c2ccbc7015553a9dcabed8cd324d3327fbb20454
Kelly Kaufman <kellykaufman12@gmail.com>
2022-05-04 20:54:46 -0700

    Adds a bunch of skeletons in product file

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -27,7 +27,11 @@ const Loading = () => {
     <div className="col-md-6">
       <Skeleton height={50} width={300}/>
       <Skeleton height={75}/>
-      <Skeleton height={25}/>
+      <Skeleton height={25} width={150}/>
+      <Skeleton height={50}/>
+      <Skeleton height={150}/>
+      <Skeleton height={50} width={100}/>
+      <Skeleton height={50} width={100} style={{marginLeft}}/>
     </div>
     </>
   )

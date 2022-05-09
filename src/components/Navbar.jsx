3ff21ca3d5fea45f950e607a18714d065c459f48
Kelly Kaufman <kellykaufman12@gmail.com>
2022-05-09 14:33:34 -0700

    Adds pages folder in src

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,93 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+// Imports
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+// Created NavBar component with bootstrap styling that
+const Navbar = () => {
+  // imported context 
+  const { cart } = useContext(CartContext);
 
-export default function Navbar () {
-  const state = useSelector((state) => state.handleCart)
   return (
-   
-    <div><nav className="navbar navbar-expand-lg navbar-light bg-light bg-white py-3 shadow-sm">
-    <div className="container">
-      <NavLink className="navbar-brand fw-bold fs-4" to="/    ">Faux Comm</NavLink>
-      <button 
-      className="navbar-toggler" 
-      type="button" 
-      data-bs-toggle="collapse" 
-      data-bs-target="#navbarSupportedContent" 
-      aria-controls="navbarSupportedContent" 
-      aria-expanded="false" 
-       aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button> 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-       
-        <li className="nav-item">
-            <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
-          </li>
-        <li className="nav-item">
-            <NavLink className="nav-link" to="/products">Products</NavLink>
-          </li>
-        <li className="nav-item">
-            <NavLink className="nav-link" to="/about">About</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/contact">Contact</NavLink>
-          </li>
-        </ul>
-     <div className="buttons">
-       <NavLink to="/login" className="btn btn-outline-dark"> 
-       <i className="fa fa-sign-in-me-1" ></i>Login</NavLink>
-    <NavLink to="/register" className="btn btn-outline-dark ms-2"> 
-    <i className="fa-solid fa-user-plus me-1"></i>Register</NavLink>
-    <NavLink to="/cart" className="btn btn-outline-dark"> <i className="fa fa-shopping-cart me-1" ></i>Cart({state.length})</NavLink>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light py-3 bg-white shadow-lg">
+        <div className="container">
+          <NavLink
+            className="navbar-brand fw-bold fs-1 text-danger text-shadow"
+            to="/"
+          >
+            FauxMart
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0 fs-4 text-shadow-navlinks fw-bold">
+              <li className="nav-item">
+                <NavLink className="nav-link ms-5" aria-current="page" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link ms-5" to="/products">
+                  Products
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link ms-5" to="/about">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link ms-5" to="/contact">
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+            <div className="buttons">
+              <NavLink
+                to="/login"
+                className="btn btn-outline-danger fw-bold"
+                id="box-shadow"
+              >
+                {" "}
+                <i className="fa fa-sign-in me-1" aria-hidden="true"></i> Log-in
+              </NavLink>
+              <NavLink
+                to="/register"
+                className="btn btn-outline-danger fw-bold ms-2"
+                id="box-shadow"
+              >
+                {" "}
+                <i className="fa fa-user-plus me-1" aria-hidden="true"></i>{" "}
+                Register
+              </NavLink>
+              <NavLink
+                to="/cart"
+                className="btn btn-outline-danger fw-bold ms-2"
+                id="box-shadow"
+              >
+                {" "}
+                <i
+                  className="fa fa-shopping-cart me-1"
+                  aria-hidden="true"
+                ></i>{" "}
+                Cart ({cart.length})
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
-    </div>
-    </div>
-  </nav> 
-  </div>
-
   );
 };
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -2,25 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/font-awesome/css/font-awesome.min.css";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "../src/redux/reducer/store";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Products from "./components/Products";
+import ProductDetails from "./pages/ProductDetails";
+import NotFound from "./components/NotFound";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Checkout from "./pages/Checkout";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/products" element={<Products />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/products/:id" element={<ProductDetails />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route exact path="/checkout" element={<Checkout />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

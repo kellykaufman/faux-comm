2741a00f8ff470a62b5238502418b78d1dfaa3b1
Kelly Kaufman <kellykaufman12@gmail.com>
2022-07-25 17:38:23 -0700

    replaces signup path with register changes signup to register and removes reset password code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,36 +1,49 @@
-
-   
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-12 d-flex flex-column align-items-center mt-5 border mb-5" id="box-shadow">
-        <h1 className="display-3 fw-bolder text-center text-shadow-navlinks mt-5 text-primary" id="font2">Login</h1>
-      <form>
-        <label className="mt-5 mb-3 fs-5" id="font2">Username:</label>
-        <input type="text" placeholder="Username" />
-      <br />
-      <label className="mt-1 mb-5 fs-5" id="font2">Password:</label>
-        <input type="password" placeholder="Password" />
-        <button className="btn btn-primary fw-bold px-4 py-2 mx-3"type="submit" id="font2">Login</button>
-      </form>
+        <div
+          className="col-md-12 d-flex flex-column align-items-center mt-5 border mb-5"
+          id="box-shadow"
+        >
+          <h1
+            className="display-3 fw-bolder text-center text-shadow-navlinks mt-5 text-primary"
+            id="font2"
+          >
+            Login
+          </h1>
+          <form>
+            <label className="mt-5 mb-3 fs-5" id="font2">
+              Username:
+            </label>
+            <input type="text" placeholder="Username" />
+            <br />
+            <label className="mt-1 mb-5 fs-5" id="font2">
+              Password:
+            </label>
+            <input type="password" placeholder="Password" />
+            <button
+              className="btn btn-primary fw-bold px-4 py-2 mx-3"
+              type="submit"
+              id="font2"
+            >
+              Login
+            </button>
+          </form>
 
-      <p className="mt-5">
-        Don't have an account? <a href="/signup">Sign up</a>
-      </p>
+          <p className="mt-5">
+            Don't have an account? <a href="/register">Register</a>
+          </p>
 
-      <p className="mb-5">
-        Forgot your password? <a href="/forgot-password">Reset</a>
-      </p>
-
-      
-      
+          <p className="mb-5">
+            Forgot your password? <a href="/forgot-password">Reset</a>
+          </p>
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
